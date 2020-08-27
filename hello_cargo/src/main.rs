19f6588396c7e13a39ae635ddad91f6d9a216b56
Javi Grande Pérez <raven.berserk@gmail.com>
2020-08-27 11:14:06 +0200

    Segundo ejemplo: Creacion de un proyecto con Cargo.

---
+++ hello_cargo/src/main.rs
@@ -0,0 +1,10 @@
+fn main() {
+    // Los comandos de Cargo que se pueden emplear para compilar y/o ejecutar el proyecto son:
+    //  - cargo new <nombre>: Creara un nuevo proyecto con cargo.
+    //  - cargo build: Compilara el proyecto. Se puede ejecutar con el flag --release, para que el compilador ejecute optimizaciones.
+    //  - cargo run: Compilara y ejecutara el proyecto.
+    //  - cargo check: Comprobara que el proyecto compila, pero no creara el ejecutable.
+
+    println!("Hello, world!");
+}
+
